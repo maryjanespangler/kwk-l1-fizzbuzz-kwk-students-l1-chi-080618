e1cb27ceca88890b50af12c00d2be36dd953c51c
maryjane spangler <maryjanespangler13@gmail.com>
2018-08-07 20:30:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
-    return"FizzBuzz" 
+    "FizzBuzz" 
  elsif number % 3 == 0
-    return"Fizz"
+    "Fizz"
   elsif number % 5 == 0
-  return "Buzz"
+  "Buzz"
  end
 end
